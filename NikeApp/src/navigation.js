d55a1f936398ffdaeda40c30850cd3b5e02a6548
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-05-17 16:51:41 -0300

    Adicionando Ícone de carrinho

--- NikeApp/src/navigation.js
+++ NikeApp/src/navigation.js
@@ -4,16 +4,36 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProductScreen from "./screens/ProductsScreen";
 import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import ShoppingCart from "./screens/ShoppingCart";
+import { Pressable, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Produtos" component={ProductScreen}/>
-        <Stack.Screen name="Detalhes Do Produto" component={ProductDetailsScreen}/>
-        <Stack.Screen name="Carrinho" component={ShoppingCart}/>
+      <Stack.Navigator  >
+        <Stack.Screen
+          name="Produtos"
+          component={ProductScreen}
+          options={({navigation}) =>( {
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Carrinho")}
+                style={{ flexDirection: "row" }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <Text style={{ marginLeft: 5, fontWeight: "500" }}>1</Text>
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Detalhes Do Produto"
+          component={ProductDetailsScreen}
+          options={{ presentation: "modal" }}
+        />
+        <Stack.Screen name="Carrinho" component={ShoppingCart} />
       </Stack.Navigator>
     </NavigationContainer>
   );
