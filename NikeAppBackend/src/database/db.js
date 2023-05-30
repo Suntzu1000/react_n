9958986dac8e751b5d94a65e78f8f413c47bd37d
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-05-30 16:52:19 -0300

    Adicionando Banco de dados MongoDB

---
+++ NikeAppBackend/src/database/db.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://gabrielfootze:nObYujCMOtcFZJWa@nikeapp.7zpt8jp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient();
+const database = client.db("teste01");
+const products = database.collection("products");
+
+module.exports = {
+    products
+}
